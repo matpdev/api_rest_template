415e77dca2ab9faa11345455ab7a3322fc067e53
Matpdev <matheus2ep@gmail.com>
2022-08-28 12:14:24 -0300

    Fixed env

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 
 module.exports = {
-   dialect: "postgres",
-   host: process.env.DATABASE_HOST,
-   port: process.env.DATABASE_PORT,
-   username: process.env.DATABASE_NAME,
-   database: process.env.DATABASE_NAME,
-   password: process.env.DATABASE_PASSWD,
-   define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-   },
-   dialectOption: {
-      timezone: "America/Sao_Paulo",
-   },
-   timezone: "America/Sao_Paulo",
+  dialect: "mysql",
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_NAME,
+  database: process.env.DATABASE_DB,
+  password: process.env.DATABASE_PASSWD,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+  dialectOption: {
+    timezone: "America/Sao_Paulo",
+  },
+  timezone: "America/Sao_Paulo",
 };
